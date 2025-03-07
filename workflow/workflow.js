fc4e790ffe70eee2ab7af41cdeba00733303f077
1024nettech <123078217+1024nettech@users.noreply.github.com>
2025-03-07 10:35:24 +0800

    Update workflow.js

--- workflow/workflow.js
+++ workflow/workflow.js
@@ -1,1 +1,36 @@
-alert();
+function addTimestampToUrl(url) {
+    return url + '?v=' + new Date().getTime();
+}
+function loadScripts(urls, callback) {
+    let loadedCount = 0;
+    const totalScripts = urls.length;
+    function handleLoad() {
+        loadedCount++;
+        if (loadedCount === totalScripts && typeof callback === 'function') {
+            callback();
+        }
+    }
+    function loadNextScript(index) {
+        if (index < urls.length) {
+            const script = document.createElement('script');
+            script.src = addTimestampToUrl(urls[index]);
+            script.onload = handleLoad;
+            document.head.appendChild(script);
+        } else {
+            handleLoad();
+        }
+    }
+    loadNextScript(0);
+}
+const scriptUrls = [
+    "https://cdnjs.cloudflare.com/ajax/libs/jquery/4.0.0-beta.2/jquery.min.js",
+    "https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js"
+];
+loadScripts(scriptUrls, function () {
+    console.log('所有脚本已加载并执行完毕！');
+    // 在这里可以安全地调用所有加载的脚本中定义的函数或其他操作
+    const url = location.href;
+    if (url.indexOf("https://detail.1688.com/offer/") != -1) {
+        alert();
+    }
+});
