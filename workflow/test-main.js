a3250b949314fe2c8f47aed9be89953eac7c7b55
1024nettech <123078217+1024nettech@users.noreply.github.com>
2025-05-13 11:08:52 +0800

    Update test-main.js

--- workflow/test-main.js
+++ workflow/test-main.js
@@ -1,6 +1,4 @@
 let b = 1;
 a(b);
 
-    let urls = ["https://1024nettech.github.io/workflow/test-func.js", "https://1024nettech.github.io/workflow/test-main.js"];
-    loadScripts(urls, 1);
-console.log(url);
+
