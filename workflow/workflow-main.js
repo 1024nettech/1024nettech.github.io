88d3f94aa411799b662c70881b6dad9734258502
1024nettech <123078217+1024nettech@users.noreply.github.com>
2025-05-13 18:01:32 +0800

    Update workflow-main.js

--- workflow/workflow-main.js
+++ workflow/workflow-main.js
@@ -2,4 +2,4 @@ const url = location.href;
 update();
 let urls = ["https://1024nettech.github.io/workflow/workflow-func.js", "https://1024nettech.github.io/workflow/workflow-main.js"];
 loadScripts(urls, 1);
-alert(1);
+alert(10);
