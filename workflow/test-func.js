62f59f7df9c013f1d97fd56c33620c9b9d084437
1024nettech <123078217+1024nettech@users.noreply.github.com>
2025-05-16 05:26:03 +0800

    Update test-func.js

--- workflow/test-func.js
+++ workflow/test-func.js
@@ -3,6 +3,7 @@ export const greet = () => {
     console.log("Hello from test-func.js!");
 };
 
-export const value = 42;
+export let value = 42;
 
-export const value0 = 42;
+
+setTimeout(() => { alert(value) }, 10000)
