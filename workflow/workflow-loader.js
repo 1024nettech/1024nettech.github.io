486eb16fc2cbde9a67c60c3207b148ea4863ff5c
1024nettech <123078217+1024nettech@users.noreply.github.com>
2025-05-15 09:58:35 +0800

    Update workflow-loader.js

--- workflow/workflow-loader.js
+++ workflow/workflow-loader.js
@@ -2,15 +2,7 @@ window.addEventListener('urlSet', () => {
     console.log(window.url);  // 读取并使用主脚本中的 url 变量
 });
 
-function cc() {
-    GM_xmlhttpRequest({
-        type: "GET",
-        url: `http://admin.qipeiyigou.com/shops/shops_add.php?shops_id=24455853`,
-        onload: function (response) {
-            console.log("来自0main cc" + response.responseText);
-        }
-    });
-}
+
 GM_xmlhttpRequest({
     type: "GET",
     url: `http://admin.qipeiyigou.com/shops/shops_add.php?shops_id=24455853`,
