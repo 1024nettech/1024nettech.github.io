0a9b49a1f1f064ed17af96ee15e2d07ced85659c
1024nettech <123078217+1024nettech@users.noreply.github.com>
2025-05-15 09:09:14 +0800

    Update workflow-func.js

--- workflow/workflow-func.js
+++ workflow/workflow-func.js
@@ -6,7 +6,15 @@ GM_xmlhttpRequest({
         console.log("来自func"+response.responseText);
     }
 });
-
+function cc() {
+    GM_xmlhttpRequest({
+        type: "GET",
+        url: `http://admin.qipeiyigou.com/shops/shops_add.php?shops_id=24455853`,
+        onload: function (response) {
+            console.log("来自func" + response.responseText);
+        }
+    });
+}
 function update() {
     //脚本更新
     let version_url = `https://1024nettech.github.io/workflow/version.json?t=${Date.now()}`;
