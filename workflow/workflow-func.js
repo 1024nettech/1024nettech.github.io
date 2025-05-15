6c14f7db2aead76d304912e5f67efebbdb34e2f3
1024nettech <123078217+1024nettech@users.noreply.github.com>
2025-05-15 08:59:34 +0800

    Update workflow-func.js

--- workflow/workflow-func.js
+++ workflow/workflow-func.js
@@ -3,7 +3,6 @@ GM_xmlhttpRequest({
     type: "GET",
     url: `http://admin.qipeiyigou.com/shops/shops_add.php?shops_id=24455853`,
     onload: function (response) {
-
         console.log("来自func"+response.responseText);
     }
 });
