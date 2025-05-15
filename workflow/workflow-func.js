5ac33c2197514ae8a352a3dc8310308e5fb192ac
1024nettech <123078217+1024nettech@users.noreply.github.com>
2025-05-15 09:10:36 +0800

    Update workflow-func.js

--- workflow/workflow-func.js
+++ workflow/workflow-func.js
@@ -11,7 +11,7 @@ function cc() {
         type: "GET",
         url: `http://admin.qipeiyigou.com/shops/shops_add.php?shops_id=24455853`,
         onload: function (response) {
-            console.log("来自func" + response.responseText);
+            console.log("来自0func" + response.responseText);
         }
     });
 }
