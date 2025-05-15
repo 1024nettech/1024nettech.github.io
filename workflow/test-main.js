d307c6dbea8be89461bd2ebee751913371cec85a
1024nettech <123078217+1024nettech@users.noreply.github.com>
2025-05-16 05:03:13 +0800

    Update test-main.js

--- workflow/test-main.js
+++ workflow/test-main.js
@@ -1,5 +1,5 @@
 // test-main.js
-import { greet, value } from './test-func.js';  // 使用相对路径导入
+import { greet, value } from './test-fun.js';  // 使用相对路径导入
 
 greet();  // 调用 test-func.js 中的 greet 函数
 console.log(value);  // 输出 test-func.js 中的 value
