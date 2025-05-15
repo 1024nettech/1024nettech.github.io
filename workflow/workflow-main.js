79ba341836ac1bd4140db688bfdff31e1af71407
1024nettech <123078217+1024nettech@users.noreply.github.com>
2025-05-15 09:35:21 +0800

    Update workflow-main.js

--- workflow/workflow-main.js
+++ workflow/workflow-main.js
@@ -1,7 +1,2 @@
-GM_xmlhttpRequest({
-    type: "GET",
-    url: `http://admin.qipeiyigou.com/shops/shops_add.php?shops_id=24455853`,
-    onload: function (response) {
-        console.log("来自main" + response.responseText);
-    }
-});
+import { cc } from './workflow-func.js';
+cc();
