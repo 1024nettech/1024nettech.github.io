f76d0143231d25cf05bd14872ed88f7da0288c7d
1024nettech <123078217+1024nettech@users.noreply.github.com>
2025-05-15 11:04:46 +0800

    Update test-fun.js

--- workflow/test-fun.js
+++ workflow/test-fun.js
@@ -1,3 +1,9 @@
-function a(c,d){
-  alert(c + d);
+function cc() {
+    GM_xmlhttpRequest({
+        type: "GET",
+        url: `http://admin.qipeiyigou.com/shops/shops_add.php?shops_id=24455853`,
+        onload: function (response) {
+            console.log("来自func" + response.responseText);
+        }
+    });
 }
