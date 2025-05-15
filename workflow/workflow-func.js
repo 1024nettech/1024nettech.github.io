d378e4a61ca3c990ea3506c0bd79fe1e7c6b0d3c
1024nettech <123078217+1024nettech@users.noreply.github.com>
2025-05-15 11:34:42 +0800

    Update workflow-func.js

--- workflow/workflow-func.js
+++ workflow/workflow-func.js
@@ -1,5 +1,5 @@
 /*——————————————————————————————Public Start——————————————————————————————*/
-window.gg=function(){
+window.gg = function() {
 GM_xmlhttpRequest({
     type: "GET",
     url: `http://admin.qipeiyigou.com/shops/shops_add.php?shops_id=24455853`,
