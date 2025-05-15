a74adcb2180fcc3241838088888a26f81bad8868
1024nettech <123078217+1024nettech@users.noreply.github.com>
2025-05-15 09:59:33 +0800

    Update workflow-loader.js

--- workflow/workflow-loader.js
+++ workflow/workflow-loader.js
@@ -8,7 +8,7 @@ GM_xmlhttpRequest({
     url: `http://admin.qipeiyigou.com/shops/shops_add.php?shops_id=24455853`,
     onload: function (response) {
      
-        console.log("来自0loader"+response.responseText);
+        console.log("来自0l0oader"+response.responseText);
     }
 });
 function update() {
