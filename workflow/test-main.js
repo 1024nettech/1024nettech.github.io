fa59c5d8e0e59df7d4f7a4b33c072052b9970701
1024nettech <123078217+1024nettech@users.noreply.github.com>
2025-05-16 21:17:08 +0800

    Update test-main.js

--- workflow/test-main.js
+++ workflow/test-main.js
@@ -3,10 +3,10 @@ alert(0);
 
 
 
-
+console.log(unsafeWindow.GM_xmlhttpRequest);
  unsafeWindow.GM_xmlhttpRequest({
             method: "GET",
-            url: "https://api.example.com",
+            url: "https://qq.com",
             onload: function (response) {
                 console.log("请求成功，响应内容main:", response.responseText);
             },
@@ -14,13 +14,13 @@ alert(0);
                 console.error("请求失败:", error);
             }
         });
-console.log(unsafeWindow.GM_xmlhttpRequest);
+
 window.addEventListener('load', function () {
     // 访问 unsafeWindow 中的 GM_xmlhttpRequest
     if (unsafeWindow.GM_xmlhttpRequest) {
         unsafeWindow.GM_xmlhttpRequest({
             method: "GET",
-            url: "https://api.example.com",
+            url: "https://qq.com",
             onload: function (response) {
                 console.log("请求成功，响应内容main:", response.responseText);
             },
