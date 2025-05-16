9f2403d553872fc30cf77e62ebea24fdb43325ca
1024nettech <123078217+1024nettech@users.noreply.github.com>
2025-05-16 20:45:35 +0800

    Update workflow-loader0.js

--- workflow/workflow-loader0.js
+++ workflow/workflow-loader0.js
@@ -16,7 +16,10 @@ unsafeWindow.GM_xmlhttp = function(url, cookie, doCallback) {
         }
     });
 };
-
+// 使用 unsafeWindow 调用 GM_xmlhttp
+unsafeWindow.GM_xmlhttp("https://qq.com", "", function(response) {
+    console.log("请求成功，响应内容loader:", response.responseText);
+});
 
 
 
