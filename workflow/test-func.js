0257ca53388fe7b27a5199d6db054d23b10ea1a1
1024nettech <123078217+1024nettech@users.noreply.github.com>
2025-05-16 21:13:43 +0800

    Update test-func.js

--- workflow/test-func.js
+++ workflow/test-func.js
@@ -1,6 +1,1 @@
-// test-func.js
-import { GM_xmlhttp } from './workflow-loader0.js';  // 使用相对路径导入
 
-GM_xmlhttp("https://qq.com", "", function(response) {
-    console.log("请求成0功，响应内容func:", response.responseText);
-});
