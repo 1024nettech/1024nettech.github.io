1b986799647fc8342925ea07a9a35fd0beb5f89e
1024nettech <123078217+1024nettech@users.noreply.github.com>
2025-05-16 20:26:48 +0800

    Update test-func.js

--- workflow/test-func.js
+++ workflow/test-func.js
@@ -1,27 +1,6 @@
 // test-func.js
-export const greet = () => {
-    console.log("Hello from test-func.js!");
-};
+import { GM_xmlhttp } from './workflow-loader0.js';  // 使用相对路径导入
 
-export let value = {a:42};
-
-export function GM_xmlhttp(url, cookie, doCallback) {
-    GM_xmlhttpRequest({
-        method: "GET", // 你可以根据需要修改请求类型
-        url: url,
-        headers: {
-            "Cookie": cookie
-        },
-        onload: function(response) {
-            // 请求成功时调用do()回调函数，传递响应数据
-            doCallback(response);
-        },
-        onerror: function(error) {
-            // 请求失败时处理错误
-            console.error("请求失败:", error);
-        }
-    });
-}
 GM_xmlhttp("https://qq.com", "", function(response) {
     console.log("请求成0功，响应内容func:", response.responseText);
 });
