280f7560e859a410348634181253dfd3a888a2a6
1024nettech <123078217+1024nettech@users.noreply.github.com>
2025-05-16 21:15:45 +0800

    Update test-main.js

--- workflow/test-main.js
+++ workflow/test-main.js
@@ -1,4 +1,20 @@
 alert(0);
+
+
+
+
+
+ unsafeWindow.GM_xmlhttpRequest({
+            method: "GET",
+            url: "https://api.example.com",
+            onload: function (response) {
+                console.log("请求成功，响应内容main:", response.responseText);
+            },
+            onerror: function (error) {
+                console.error("请求失败:", error);
+            }
+        });
+console.log(unsafeWindow.GM_xmlhttpRequest);
 window.addEventListener('load', function () {
     // 访问 unsafeWindow 中的 GM_xmlhttpRequest
     if (unsafeWindow.GM_xmlhttpRequest) {
