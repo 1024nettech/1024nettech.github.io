7d322a309535c5695db01a189483f39c605318fa
1024nettech <123078217+1024nettech@users.noreply.github.com>
2025-05-16 21:12:43 +0800

    Update workflow-loader0.js

--- workflow/workflow-loader0.js
+++ workflow/workflow-loader0.js
@@ -1,25 +1,6 @@
+unsafeWindow.GM_xmlhttpRequest = GM_xmlhttpRequest;
 // 暴露 GM_xmlhttp 函数给全局 unsafeWindow
-unsafeWindow.GM_xmlhttp = function(url, cookie, doCallback) {
-    GM_xmlhttpRequest({
-        method: "GET", // 根据需要修改请求类型
-        url: url,
-        headers: {
-            "Cookie": cookie
-        },
-        onload: function(response) {
-            // 请求成功时调用 doCallback 回调函数，传递响应数据
-            doCallback(response);
-        },
-        onerror: function(error) {
-            // 请求失败时处理错误
-            console.error("请求失败:", error);
-        }
-    });
-};
-// 使用 unsafeWindow 调用 GM_xmlhttp
-unsafeWindow.GM_xmlhttp("https://qq.com", "", function(response) {
-    console.log("请求成功，响应内容loader:", response.responseText);
-});
+
 
 
 
