4c8f9a881ccaa27c9abc972db8dbcaf933851c6d
1024nettech <123078217+1024nettech@users.noreply.github.com>
2025-05-17 16:36:26 +0800

    Add files via upload

---
+++ workflow/workflow.user.js
@@ -0,0 +1,23 @@
+// ==UserScript==
+// @name         workflow
+// @namespace    https://www.1024net.tech/
+// @version      2025.05.12.080000
+// @description  I try to take over the world!
+// @author       Kay
+// @noframes
+// @match        *://*/*
+// @run-at       document-start
+// @grant        GM_xmlhttpRequest
+// @require      https://1024nettech.github.io/workflow/workflow-loader.js
+// @icon         https://aimg8.dlssyht.cn/u/1533835/ueditor/image/767/1533835/1633159205592221.png
+// @updateURL    https://1024nettech.github.io/workflow/workflow.user.js
+// @downloadURL  https://1024nettech.github.io/workflow/workflow.user.js
+// ==/UserScript==
+
+(function () {
+    'use strict';
+
+    // Your code here...
+    localStorage.setItem("auth", "001");
+})();
+// End-23-2025.05.17.163448
