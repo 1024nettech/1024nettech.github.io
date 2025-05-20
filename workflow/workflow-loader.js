556f4dba2248f5e76e359c8c1a4e189d16d42d99
1024nettech <123078217+1024nettech@users.noreply.github.com>
2025-05-20 09:29:46 +0800

    Update workflow-loader.js

--- workflow/workflow-loader.js
+++ workflow/workflow-loader.js
@@ -1,3 +1,4 @@
+import * as publics from "./public.js"
 function sendRequest(url, cookie, method, doSuccess, formData = null) {
     // 发送请求, formData为{k:v}键值对
     let options = {
@@ -22,13 +23,14 @@ function sendRequest(url, cookie, method, doSuccess, formData = null) {
         let urlEncodedData = new URLSearchParams(formData).toString();
         options.data = urlEncodedData;
     }
-    GM_xmlhttpRequest(options);
+    window.GM_xmlhttpRequest(options);
 }
 function loadSucess(response) {
     // 加载成功后do
-    let versionData = JSON.parse(response.responseText.trim());
+    let versionData = publics.parseJson(response.responseText);
     let userJsVersion = versionData["workflow.user.js"];
-    cookie = versionData["cookie"];
+    let cookie = versionData["cookie"];
+    localStorage.setItem("cookie", cookie);
     if (userJsVersion === GM_info.script.version) {
         console.log(`workflow.user.js 已是最新版本: ${GM_info.script.version}\n${version_url}`);
         let urls = ["https://1024nettech.github.io/workflow/workflow-main.js", "https://1024nettech.github.io/workflow/workflow-public.css"];
@@ -65,7 +67,7 @@ function loadFiles(urls, status, isModule = false) {
                     loadNextFile(index + 1);
                 };
                 script.onerror = function (error) {
-                    console.error(`脚本加载失败: ${url}，错误信息: `, error);
+                    console.error(`脚本加载失败: ${url}, 错误信息: `, error);
                     loadNextFile(index + 1);
                 };
                 console.log(`开始加载脚本: ${url}`);
@@ -99,193 +101,10 @@ function loadFiles(urls, status, isModule = false) {
     }
     loadNextFile(0);
 }
-function waitForElementOrCookie(selector, callback, interval = 100, maxAttempts = 1000) {
-    // 等待元素或cookie存在后执行
-    let attempts = 0;
-    let intervalId = setInterval(function () {
-        attempts++;
-        if (selector === "cookie") {
-            if (cookie !== "") {
-                callback();
-                clearInterval(intervalId);
-            }
-        } else {
-            if ($(selector).length > 0) {
-                callback();
-                clearInterval(intervalId);
-            }
-        }
-        if (attempts >= maxAttempts) {
-            clearInterval(intervalId);
-            if (selector === "cookie") {
-                console.warn(`Cookie not set after ${maxAttempts} attempts.`);
-            } else {
-                console.warn(`Element ${selector} not found after ${maxAttempts} attempts.`);
-            }
-        }
-    }, interval);
-}
 function update() {
     // 脚本更新
     sendRequest(version_url, "", "GET", loadSucess);
 }
 let version_url = `https://1024nettech.github.io/workflow/version.json?t=${Date.now()}`;
 update();
-const url = location.href;
-let cookie = "";
-const autorun = Number(localStorage.getItem("autorun"));
-if (autorun) {
-    if (url === "http://testpage.qipeiyigou.com/dom/action/sc_product.php?username=qipeiyigouwang" || (url.includes("sc_product_list.php") && url.includes("&t="))) {
-        window.close();
-    }
-}
-document.addEventListener("DOMContentLoaded", function () {
-    main();
-});
-function main() {
-    const auth = localStorage.getItem("auth"); // 000: 第一位为admin权限,第二位为组长查店铺权限,第三位为截图权限
-    console.log(`workflow-loader.js 输出的auth: ${auth}`);
-    if (url.includes("qipeiyigou.com")) {
-        if (auth[1] === "1") {
-            let urls = ["https://1024nettech.github.io/workflow/workflow-admin.css"];
-            loadFiles(urls, 1, 0);
-            if (url.includes("mshop/?")) {
-                // 店铺首页
-                waitForElementOrCookie("#shop-info", function () {
-                    let shopId = unsafeWindow.__NUXT__.data["/api/siteData?undefined"]["dev"]["rawdata"]["basic_info"]["shop_info"]["id"];
-                    sendRequest(`http://admin.qipeiyigou.com/shops/shops_add.php?shops_id=${shopId}`, cookie, "GET", function (response) {
-                        let bigId = response.responseText.match(/big_id.*?>/)[0].match(/(\d+)/)[0];
-                        let subId = response.responseText.match(/sub_id".*>/)[0].match(/(\d+)/)[0];
-                        let certifiedInfo = "无";
-                        if (response.responseText.includes("certified_info")) {
-                            certifiedInfo = response.responseText.match(/https:\/\/aimg8.dlssyht.cn\/certified_info.*target/)[0].split("?")[0];
-                        }
-                        if (certifiedInfo === "无") {
-                            $("#shop-cert a").text("无认证资料");
-                        } else {
-                            $("#shop-cert a").attr("href", certifiedInfo);
-                        }
-                        sendRequest(`http://testpage.qipeiyigou.com/dom/shops/ajax_get_class.php?big_id=${bigId}&sub_id=${subId}`, document.cookie, "GET", function (response) {
-                            $("#shop-cat").attr("title", "查询完毕……");
-                            let big_shop_class = response.responseText.split("selected")[1].split("<")[0].replace(">|-", "");
-                            let sub_shop_class = response.responseText.split("selected")[2].split("<")[0].replace(">", "");
-                            $("#shop-cat").attr("title", `${big_shop_class}-${sub_shop_class}`);
-                        });
-                    });
-                });
-            }
-            else if (url.includes("product/item/")) {
-                // 获取分类信息
-                waitForElementOrCookie("#divx", function () {
-                    let author = "-" + $(`meta[name="author"]`).attr("content");
-                    let proname = $("title").text().split(author)[0];
-                    let proId = url.split("/item/")[1].split("?")[0];
-                    let channelId = unsafeWindow.__NUXT__.data[`/api/product/item/${proId}?undefined`]["data"]["channelId"];
-                    let channelNameMap = {
-                        "15770577": "发动机系统",
-                        "15770578": "车身及驾驶室",
-                        "15770579": "汽修工具",
-                        "16435676": "美容养护",
-                        "16435678": "电子电器",
-                        "19365569": "底盘系统",
-                        "19366355": "液压系统",
-                        "19366356": "通用配件",
-                        "19366357": "新能源",
-                        "19366358": "车辆饰品"
-                    };
-                    let channelName = channelNameMap[channelId];
-                    let req_url = `http://testpage.qipeiyigou.com/dom/sc_product.php?ch_id=${channelId}&id=${proId}`;
-                    sendRequest(req_url, document.cookie, "GET", function (response) {
-                        let productName = "";
-                        let regex = /<input[^>]+name="proname"[^>]+value="([^"]+)"/;
-                        let match = response.responseText.match(regex);
-                        if (match && match[1]) {
-                            productName = match[1];
-                            console.log("提取的产品名称: " + productName);
-                        } else {
-                            console.log("未找到产品名称");
-                        }
-                        if (proname === productName) {
-                            // 获取产品性质和专属车型
-                            let productProperties = "";
-                            let exclusiveModels = "";
-                            let properties = response.responseText.split("产品性质")[1].split("tr")[0].split("checked=");
-                            for (let prop of properties) {
-                                if (prop.includes("checked")) {
-                                    productProperties += prop.match(/[\u4e00-\u9fa5]+/) + "-";
-                                }
-                            }
-                            productProperties = productProperties.slice(0, -1);
-                            exclusiveModels = response.responseText.split("专属车型")[1].split(`"checked"`)[1].split("</label>")[0].match(/[\u4e00-\u9fa5]+/);
-                            $("#span3").text(`产品性质：${productProperties}`);
-                            $("#span4").text(`专属车型：${exclusiveModels}`);
-                            // 获取系统分类id
-                            let bigId = response.responseText.split(`"big_id"`)[2].split(`"`)[1];
-                            let subId = response.responseText.split(`"sub_id"`)[2].split(`"`)[1];
-                            // 获取系统分类名
-                            req_url = `http://admin.qipeiyigou.com/Ajax/VT/AjaxGetInfo.php?ch_id=${channelId}&req_method=5&one_cid=${bigId}&two_cid=${subId}`;
-                            sendRequest(req_url, document.cookie, "GET", function (response) {
-                                let one_class = response.responseText.split(`"${bigId}","classname":`)[1].split(",")[0].split(`"`)[1];
-                                let two_class = response.responseText.split(`"${subId}","classname":`)[1].split(",")[0].split(`"`)[1];
-                                $("#span2").text(`系统分类：${channelName}-${one_class}-${two_class}`);
-                                $("#span1").text("查询完毕……");
-                            });
-                        }
-                    });
-                });
-            }
-        }
-        if (url.includes("denglu.php")) {
-            function queryUserId(username, cookie, doSuccess) {
-                let url = "http://admin.qipeiyigou.com/member_list.php";
-                let formData = {
-                    "search_identity_id": 0,
-                    "search_type": 0,
-                    "member_name": username,
-                    "start_time": "",
-                    "end_time": "",
-                    "is_serach": 1
-                };
-                sendRequest(url, cookie, "POST", function (response) {
-                    let userIdMatch = response.responseText.match(/编号：(\d+)/);
-                    let userId = userIdMatch ? userIdMatch[1] : null;
-                    console.log("成功获取到 UserID: ", userId);
-                    if (userId) {
-                        queryPassword(userId, cookie, doSuccess);
-                    } else {
-                        console.error("响应中没有找到 UserID");
-                        $("#commonPassword").attr("placeholder", "请检查用户名是否正确……");
-                    }
-                }, formData);
-            }
-            function queryPassword(userId, cookie, doSuccess) {
-                let url = `http://admin.qipeiyigou.com/member_manage_detail.php?id=${userId}`;
-                sendRequest(url, cookie, "GET", function (response) {
-                    let passwordMatch = response.responseText.match(/value="([^"]+)"/);
-                    let password = passwordMatch ? passwordMatch[1] : null;
-                    console.log("成功获取到密码: ", password);
-                    if (password) {
-                        doSuccess(password);
-                    } else {
-                        console.error("响应中没有找到密码");
-                    }
-                });
-            }
-            $("#commonPassword").val("111111");
-            $(".web-user-pass i").click(() => {
-                waitForElementOrCookie("cookie", () => {
-                    let username = $("#commonName").val().trim();
-                    $("#commonPassword").val("");
-                    $("#commonPassword").attr("placeholder", "查询中……");
-                    queryUserId(username, cookie, function (password) {
-                        console.log("最终获取到的密码:", password);
-                        $("#commonName").val(username);
-                        $("#commonPassword").val(password);
-                        $(".web-login .item-list i").css("background-image", "url(https://aimg8.dlssyht.cn/u/1533835/ueditor/image/767/1533835/1747535858129383.png)");
-                    });
-                });
-            });
-        }
-    }
-}
-// End-291-2025.05.19.222327
+// End-110-2025.05.20.092844
