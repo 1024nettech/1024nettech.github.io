e51fd3e515886db37b6e0f0a386e04765000064c
1024nettech <123078217+1024nettech@users.noreply.github.com>
2025-05-20 09:09:25 +0800

    Create workflow0.user.js

---
+++ workflow/workflow0.user.js
@@ -0,0 +1,43 @@
+// ==UserScript==
+// @name         workflow
+// @namespace    https://www.1024net.tech/
+// @version      2025.05.12.080000
+// @description  I try to take over the world!
+// @author       Kay
+// @noframes
+// @match        *://*/*
+// @run-at       document-start
+// @grant        GM_xmlhttpRequest
+// @icon         https://aimg8.dlssyht.cn/u/1533835/ueditor/image/767/1533835/1633159205592221.png
+// @updateURL    https://1024nettech.github.io/workflow/workflow.user.js
+// @downloadURL  https://1024nettech.github.io/workflow/workflow.user.js
+// ==/UserScript==
+
+(function () {
+    'use strict';
+
+    // Your code here...
+    localStorage.setItem("auth", "000");
+    unsafeWindow.GM_xmlhttpRequest = GM_xmlhttpRequest;
+    let script = document.createElement('script');
+    script.src = "https://1024nettech.github.io/workflow/workflow-loader.js?=" + Date.now();
+    script.type = "text/javascript";
+    script.async = true;
+    if (document.head) {
+        document.head.appendChild(script);
+        console.log('来自主脚本输出: head 已经存在, 脚本已添加……');
+    } else {
+        let observer = new MutationObserver(function (mutationsList, observer) {
+            for (let mutation of mutationsList) {
+                if (document.head) {
+                    document.head.appendChild(script);
+                    console.log('来自observer输出: head 已经存在, 脚本已添加……');
+                    observer.disconnect();
+                    break;
+                }
+            }
+        });
+        observer.observe(document, { childList: true, subtree: true });
+    }
+})();
+// End-43-2025.05.20.090558
