613343d1e435291e269cef5b7bdbc04092488f0f
1024nettech <123078217+1024nettech@users.noreply.github.com>
2025-05-20 10:01:57 +0800

    Update workflow-loader.js

--- workflow/workflow-loader.js
+++ workflow/workflow-loader.js
@@ -1,30 +1,4 @@
 import * as publics from "./public.js"
-function sendRequest(url, cookie, method, doSuccess, formData = null) {
-    // 发送请求, formData为{k:v}键值对
-    let options = {
-        method: method,
-        url: url,
-        headers: {
-            "Cookie": cookie
-        },
-        onload: function (response) {
-            if (response.status === 200) {
-                doSuccess(response);
-            } else {
-                console.error("请求失败, 状态码: " + response.status);
-            }
-        },
-        onerror: function (error) {
-            console.error("请求发生错误: ", error);
-        }
-    };
-    if (method.toUpperCase() === "POST" && formData) {
-        options.headers["Content-Type"] = "application/x-www-form-urlencoded";
-        let urlEncodedData = new URLSearchParams(formData).toString();
-        options.data = urlEncodedData;
-    }
-    window.GM_xmlhttpRequest(options);
-}
 function loadSucess(response) {
     // 加载成功后do
     let versionData = publics.parseJson(response.responseText);
@@ -34,77 +8,19 @@ function loadSucess(response) {
     if (userJsVersion === GM_info.script.version) {
         console.log(`workflow.user.js 已是最新版本: ${GM_info.script.version}\n${version_url}`);
         let urls = ["https://1024nettech.github.io/workflow/workflow-main.js", "https://1024nettech.github.io/workflow/workflow-public.css"];
-        loadFiles(urls, 1, 1);
+        publics.loadFiles(urls, 1, 1);
         if (location.href.includes("1688.com")) {
             urls = ["https://cdnjs.cloudflare.com/ajax/libs/jquery/4.0.0-beta.2/jquery.min.js", "https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js"];
-            loadFiles(urls, 0, 0);
+            publics.loadFiles(urls, 0, 0);
         }
     } else {
         $("body").html(`<a id="update_tip" href="https://1024nettech.github.io/workflow/workflow.user.js" target="_blank">点击更新</a>`);
     }
 }
-function loadFiles(urls, status, isModule = false) {
-    // 动态加载外部文件(JS/CSS)
-    if (status === 1) {
-        urls = urls.map(url => url + "?t=" + Date.now());
-    }
-    let totalFiles = urls.length;
-    let loadedFiles = 0;
-    function loadNextFile(index) {
-        if (index < totalFiles) {
-            let url = urls[index];
-            let fileExtension = url.split(".").pop().split("?")[0].toLowerCase();
-            if (fileExtension === "js") {
-                let script = document.createElement("script");
-                script.src = url;
-                script.type = isModule ? "module" : "text/javascript";
-                script.onload = function () {
-                    console.log(`脚本加载完成: ${url}`);
-                    loadedFiles++;
-                    if (loadedFiles === totalFiles) {
-                        onFilesLoaded();
-                    }
-                    loadNextFile(index + 1);
-                };
-                script.onerror = function (error) {
-                    console.error(`脚本加载失败: ${url}, 错误信息: `, error);
-                    loadNextFile(index + 1);
-                };
-                console.log(`开始加载脚本: ${url}`);
-                document.head.append(script);
-            } else if (fileExtension === "css") {
-                let link = document.createElement("link");
-                link.rel = "stylesheet";
-                link.href = url;
-                link.onload = function () {
-                    console.log(`CSS 加载完成: ${url}`);
-                    loadedFiles++;
-                    if (loadedFiles === totalFiles) {
-                        onFilesLoaded();
-                    }
-                    loadNextFile(index + 1);
-                };
-                link.onerror = function (error) {
-                    console.error(`CSS 加载失败: ${url}, 错误信息: `, error);
-                    loadNextFile(index + 1);
-                };
-                console.log(`开始加载 CSS: ${url}`);
-                document.head.append(link);
-            } else {
-                console.error(`无法识别的文件类型: ${url}`);
-                loadNextFile(index + 1);
-            }
-        }
-    }
-    function onFilesLoaded() {
-        console.log("所有文件加载完成!");
-    }
-    loadNextFile(0);
-}
 function update() {
     // 脚本更新
-    sendRequest(version_url, "", "GET", loadSucess);
+    publics.sendRequest(version_url, "", "GET", loadSucess);
 }
 let version_url = `https://1024nettech.github.io/workflow/version.json?t=${Date.now()}`;
 update();
-// End-110-2025.05.20.092844
+// End-26-2025.05.20.100042
