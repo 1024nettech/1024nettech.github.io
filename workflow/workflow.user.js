edbdfe497a57f3becab6568d80fa1efab868f58c
1024nettech <123078217+1024nettech@users.noreply.github.com>
2025-05-22 10:02:59 +0800

    Update workflow.user.js

--- workflow/workflow.user.js
+++ workflow/workflow.user.js
@@ -6,6 +6,7 @@
 // @author       Kay
 // @noframes
 // @match        *://*/*
+// @connect *
 // @run-at       document-start
 // @grant        GM_xmlhttpRequest
 // @icon         https://aimg8.dlssyht.cn/u/1533835/ueditor/image/767/1533835/1633159205592221.png
@@ -20,4 +21,4 @@
     localStorage.setItem("auth", "000");
     const _0x4ee138 = _0xcc84; (function (_0x59240b, _0x2969ff) { const _0x576b86 = _0xcc84, _0xfaca7f = _0x59240b(); while (!![]) { try { const _0x910dfd = -parseInt(_0x576b86(0x18c)) / 0x1 + parseInt(_0x576b86(0x186)) / 0x2 + parseInt(_0x576b86(0x197)) / 0x3 * (parseInt(_0x576b86(0x183)) / 0x4) + parseInt(_0x576b86(0x18d)) / 0x5 + -parseInt(_0x576b86(0x188)) / 0x6 * (parseInt(_0x576b86(0x185)) / 0x7) + -parseInt(_0x576b86(0x189)) / 0x8 * (parseInt(_0x576b86(0x198)) / 0x9) + parseInt(_0x576b86(0x187)) / 0xa; if (_0x910dfd === _0x2969ff) break; else _0xfaca7f['push'](_0xfaca7f['shift']()); } catch (_0xae3b30) { _0xfaca7f['push'](_0xfaca7f['shift']()); } } }(_0x4ffc, 0x523eb), unsafeWindow[_0x4ee138(0x184)] = GM_info, unsafeWindow[_0x4ee138(0x18f)] = GM_xmlhttpRequest); function _0x4ffc() { const _0x2c0b0e = ['observe', 'GM_xmlhttpRequest', 'disconnect', 'src', 'head', 'async', 'log', '来自主脚本输出:\x20head\x20已经存在,\x20脚本已添加……', 'https://1024nettech.github.io/workflow/workflow-loader.js?=', '986259DHqrvm', '174429RVGsBu', 'script', 'now', 'type', '4ceevXd', 'GM_info', '10885EbtCns', '385362IUkGYE', '10346400wxKgzS', '2484nqxskS', '176UzeDJh', 'appendChild', '来自observer输出:\x20head\x20已经存在,\x20脚本已添加……', '537414KGfiHJ', '1941835JQubnZ']; _0x4ffc = function () { return _0x2c0b0e; }; return _0x4ffc(); } let script = document['createElement'](_0x4ee138(0x199)); function _0xcc84(_0x1f9589, _0x208eeb) { const _0x4ffccf = _0x4ffc(); return _0xcc84 = function (_0xcc8463, _0x2d0d93) { _0xcc8463 = _0xcc8463 - 0x181; let _0x8b83de = _0x4ffccf[_0xcc8463]; return _0x8b83de; }, _0xcc84(_0x1f9589, _0x208eeb); } script[_0x4ee138(0x191)] = _0x4ee138(0x196) + Date[_0x4ee138(0x181)](), script[_0x4ee138(0x182)] = 'module', script[_0x4ee138(0x193)] = !![]; if (document[_0x4ee138(0x192)]) document[_0x4ee138(0x192)][_0x4ee138(0x18a)](script), console['log'](_0x4ee138(0x195)); else { let observer = new MutationObserver(function (_0x69d66f, _0x5a7f78) { const _0x3fd110 = _0x4ee138; for (let _0x151506 of _0x69d66f) { if (document[_0x3fd110(0x192)]) { document['head'][_0x3fd110(0x18a)](script), console[_0x3fd110(0x194)](_0x3fd110(0x18b)), _0x5a7f78[_0x3fd110(0x190)](); break; } } }); observer[_0x4ee138(0x18e)](document, { 'childList': !![], 'subtree': !![] }); }
 })();
-// End-44-2025.05.20.160253
+// End-45-2025.05.22.100219
