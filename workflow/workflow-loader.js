393e3ba9d1ffe71918785032b2478c8e6e956ceb
1024nettech <123078217+1024nettech@users.noreply.github.com>
2025-05-23 15:47:51 +0800

    Update workflow-loader.js

--- workflow/workflow-loader.js
+++ workflow/workflow-loader.js
@@ -13,15 +13,13 @@ async function loadSucess(response) {
     let userJsVersion = versionData["workflow.user.js"];
     if (userJsVersion === window.GM_info.script.version) {
         console.log(`workflow.user.js 已是最新版本: ${GM_info.script.version}\n${version_url}`);
-        let urls1 = ["https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.18.5/xlsx.full.min.js", "https://cdnjs.cloudflare.com/ajax/libs/crypto-js/4.2.0/crypto-js.min.js"];
-        await publics.loadFiles(urls1, 0, 0);
-        let urls0 = ["https://1024nettech.github.io/workflow/workflow-main.js", "https://1024nettech.github.io/workflow/workflow-public.css"];
-        setTimeout(() => { publics.loadFiles(urls0, 1, 1); }, 1000);
-        // let urls0 = ["https://1024nettech.github.io/workflow/workflow-main.js", "https://1024nettech.github.io/workflow/workflow-public.css"];
-        // await publics.loadFiles(urls0, 0, 1);
+        let urls = ["https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.18.5/xlsx.full.min.js", "https://cdnjs.cloudflare.com/ajax/libs/crypto-js/4.2.0/crypto-js.min.js"];
+        await publics.loadFiles(urls, 0, 0);
+        urls = ["https://1024nettech.github.io/workflow/workflow-main.js", "https://1024nettech.github.io/workflow/workflow-public.css"];
+        await publics.loadFiles(urls, 1, 1);
         if (location.href.includes("1688.com")) {
-            let urls2 = ["https://cdnjs.cloudflare.com/ajax/libs/jquery/4.0.0-beta.2/jquery.min.js", "https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js"];
-            await publics.loadFiles(urls2, 0, 0);
+            urls = ["https://cdnjs.cloudflare.com/ajax/libs/jquery/4.0.0-beta.2/jquery.min.js", "https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js"];
+            await publics.loadFiles(urls, 0, 0);
         }
         if (url.includes("qipeiyigou.com")) {
             let cookie = localStorage.getItem("cookie");
@@ -40,4 +38,4 @@ function update() {
 }
 let version_url = `https://1024nettech.github.io/workflow/version.json?t=${Date.now()}`;
 update();
-// End-41-2025.05.23.145243
+// End-41-2025.05.23.154730
