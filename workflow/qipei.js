7c2596bd1645803973e5f164d001aeed8bf0b528
1024nettech <123078217+1024nettech@users.noreply.github.com>
2025-05-25 13:23:08 +0800

    Update qipei.js

--- workflow/qipei.js
+++ workflow/qipei.js
@@ -1,89 +1,82 @@
 import * as publics from "./public.js"
 const url = location.href;
-export function openProductsEdit() {
-    // Esc打开产品栏目管理列表
-    // localStorage.setItem("hash", "1");
-    let page = location.href.split("&page=")[1];
-    $("a").filter(function () {
-        return $(this).text().trim() === "编辑";
-    }).each(function (index) {
-        window.open($(this).attr("href") + "#page=" + page + "-" + (index + 1), "_blank");
-    });
-    if ($(".page-next").length) {
-        location.href = $(".page-next").attr("href");
-    } else {
-        window.close();
+export function export_tsc() {
+    // 首页导出数据组件
+    let html = `
+        <img src="https://aimg8.dlssyht.cn/u/1533835/ueditor/image/767/1533835/1746954291684901.png" id="toggleImg" />
+        <input type="text" id="nameInput" placeholder="请输入姓名" />
+        <button id="exportx">导出数据为 xlsx</button>
+        <img src="https://aimg8.dlssyht.cn/u/1533835/ueditor/image/767/1533835/1747970937415077.png" id="clearLocalStorage" />
+        <input type="text" id="usernameInput" value="当前用户名: ">
+        `;
+    $("body").append(html);
+    if (url.includes("denglu.php")) { $("#toggleImg").css("display", "none"); }
+    let autorun = Number(localStorage.getItem("autorun"));
+    if (autorun) {
+        $("#toggleImg").attr("src", "https://aimg8.dlssyht.cn/u/1533835/ueditor/image/767/1533835/1746954291684901.png");
     }
-};
-export async function handleProductAction(checked_car, status = "") {
-    let today = publics.generateTimestamp(0);
-    let person = "xxpersonname";
-    let username = $(".welcome").text().trim().replace("欢迎您：", "");
-    let urlParams = new URLSearchParams(new URL(url).search);
-    let ch_id = urlParams.get("ch_id");
-    let id = urlParams.get("id");
-    let ch_name = $(".myColumnTit").text();
-    let product_link = `http://testpage.qipeiyigou.com/qipeiyigouwang/products/${id}.html${location.hash}`;
-    let record = `${today}\t${person}\t${username}\t${ch_id}\t${id}\t${ch_name}\t${product_link}\t`;
-    let labelsBefore = Array.from(document.querySelectorAll(`input[name="properties[]"]:checked`))
-        .map(checkbox => checkbox.closest("label").textContent.trim())
-        .join(", ");
-    if (status === "未处理") {
-        record += `${labelsBefore}\t${labelsBefore}\t${status}`;
-        // await waitForHashToMatch();
-        await publics.appendToRecord(record);
-        // let stored_hash = localStorage.getItem("hash");
-        // if (stored_hash) {
-        //     localStorage.setItem("hash", parseInt(stored_hash) + 1);
-        // } else {
-        //     console.error("存储的哈希值无效或缺失");
-        // }
-        window.close();
-    } else {
-        await processOtherStatus(checked_car, labelsBefore, record, status);
+    else {
+        $("#toggleImg").attr("src", "https://aimg8.dlssyht.cn/u/1533835/ueditor/image/767/1533835/1746954290554632.png");
     }
-}
-async function waitForHashToMatch() {
-    return new Promise(resolve => {
-        function checkHash() {
-            let currentHash = location.hash.split("#")[1];
-            let stored_hash = localStorage.getItem("hash");
-            if (currentHash === stored_hash) {
-                resolve();
-            } else {
-                setTimeout(checkHash, 100);
-            }
+    $("#toggleImg").click(function () {
+        let autorun = Number(localStorage.getItem("autorun"));
+        if (autorun) {
+            $(this).attr("src", "https://aimg8.dlssyht.cn/u/1533835/ueditor/image/767/1533835/1746954290554632.png");
+            autorun = 0;
+        } else {
+            $(this).attr("src", "https://aimg8.dlssyht.cn/u/1533835/ueditor/image/767/1533835/1746954291684901.png");
+            autorun = 1;
         }
-        checkHash();
+        localStorage.setItem("autorun", autorun);
     });
-}
-async function processOtherStatus(checked_car, labelsBefore, record, status) {
-    await new Promise(resolve => {
-        let interval = setTimeout(async function checkAndExecute() {
-            if ($("#sub_id option:selected").length && $("#shop_pro_class_big_id option:selected").length) {
-                // let currentHash = location.hash.split("#")[1];
-                // let stored_hash = localStorage.getItem("hash");
-                // if (currentHash === stored_hash) {
-                // localStorage.setItem("hash", parseInt(stored_hash) + 1);
-                clearTimeout(interval);
-                $("input[type=checkbox][value=4]").prop("checked", false);
-                if (checked_car) {
-                    $("input[type=checkbox][value=2]").prop("checked", true);
-                }
-                let labelsAfter = Array.from(document.querySelectorAll(`input[name="properties[]"]:checked`))
-                    .map(checkbox => checkbox.closest("label").textContent.trim())
-                    .join(", ");
-                record += `${labelsBefore}\t${labelsAfter}\t${status}`;
-                await publics.appendToRecord(record);
-                $("title").text("完成");
-                $("#submit_msg a").click();
-                resolve();
-                // }
-            } else {
-                setTimeout(checkAndExecute, 100);
-            }
-        }, 100);
+    $("#exportx").click(function () {
+        let personName = $("#nameInput").val().trim();
+        localStorage.setItem("name", personName);
+        if (!personName) {
+            alert("姓名不能为空！");
+            return;
+        }
+        let time = publics.generateTimestamp(1);
+        let fileName = `${personName}-${time}`;
+        publics.downloadRecordAsFile(personName, fileName);
     });
+    $("#clearLocalStorage").click(() => {
+        localStorage.clear();
+        $("#clearLocalStorage").attr("src", "https://aimg8.dlssyht.cn/u/1533835/ueditor/image/767/1533835/1747976911167008.png");
+        setTimeout(() => { location.reload(); }, 1000);
+    });
+    $("#usernameInput").click(() => { $("#usernameInput").val(""); });
+    let stored_personname = localStorage.getItem("name");
+    if (stored_personname) {
+        $("#nameInput").val(stored_personname);
+    }
+    $("#usernameInput").on("input", function () {
+        let usernames = $("#usernameInput").val().trim().split(" ").map(function (username) {
+            return username.trim();
+        }).filter(function (username) {
+            return username.length > 0;
+        });
+        let first_username = usernames[0] || "";
+        localStorage.setItem("usernames", usernames.join(" "));
+        $("#commonName").val(first_username);
+        $("#commonName").focus();
+        $("#commonName").prop("disabled", true);
+        $("#commonPassword").val("111111");
+        $("#commonPassword").focus();
+        $("#commonYzm").focus();
+        $("#usernameInput").val(`当前用户名: ${first_username}`);
+    });
+    let stored_usernames = localStorage.getItem("usernames");
+    if (stored_usernames) {
+        let first_stored_username = stored_usernames.split(" ")[0];
+        $("#commonName").val(first_stored_username);
+        $("#commonName").focus();
+        $("#commonName").prop("disabled", true);
+        $("#commonPassword").val("111111");
+        $("#commonPassword").focus();
+        $("#commonYzm").focus();
+        $("#usernameInput").val(`当前用户名: ${first_stored_username}`);
+    }
 }
 export function open_close_shop_products() {
     // 店铺内打开或关闭产品
@@ -93,6 +86,79 @@ export function open_close_shop_products() {
         });
     } else if (location.href.includes("mshop/product/item")) {
         window.close();
+    }
+}
+export function showKeyword() {
+    // 显示关键词
+    let keyword = $("meta[name=keywords]").attr("content");
+    let author = $("meta[name=author]").attr("content");
+    if (keyword.includes(author)) {
+        $("#keywordx").text("关键词为空");
+    } else {
+        $("#keywordx").text(keyword);
+    }
+};
+export function checkProduct() {
+    // 检查产品详情
+    let id = 0;
+    let tip = "";
+    $("#tipx").text("正在检查中……");
+    if ($(".main a").length) {
+        id = 1;
+        tip += "存在超链接！";
+    }
+    if ($(".main *[style*=pointer]").length) {
+        id = 2;
+        tip += "存在非超链接小手！";
+    }
+    let images = $(".main img");
+    for (let i = 0; i < images.length; i++) {
+        let src = images.eq(i).attr("src");
+        if (!src.includes("aimg8.dlssyht.cn")) {
+            id = 3;
+            tip += "存在外链图片！";
+            break;
+        }
+    }
+    if (id === 0) {
+        tip = "正常！";
+    } else {
+        $("#tipx").css("background-color", "red");
+        alert(tip);
+    }
+    $("#tipx").text(`检查结果: ${tip}`);
+};
+export async function fetchChIdsAndTitles(url) {
+    // 获取产品栏目id和名称{}
+    try {
+        let response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`请求失败, 状态码: ${response.status}`);
+        }
+        let arrayBuffer = await response.arrayBuffer();
+        let decoder = new TextDecoder("gbk");
+        let decodedText = decoder.decode(arrayBuffer);
+        let parser = new DOMParser();
+        let doc = parser.parseFromString(decodedText, "text/html");
+        let anchorElements = doc.querySelectorAll(`a[href*="ch_id="]`);
+        let chIdDict = {};
+        anchorElements.forEach(anchor => {
+            let chIdMatch = anchor.href.match(/ch_id=(\d+)/);
+            if (chIdMatch) {
+                let chId = chIdMatch[1];
+                let titleElement = anchor.querySelector(".p-tit");
+                let title = titleElement ? titleElement.textContent.trim() : "";
+                if (title) {
+                    chIdDict[chId] = title;
+                }
+            }
+        });
+        if (chIds) { await publics.appendToRecord("chIds", chIds); }
+        console.log("提取到的 ch_id 和标题字典: ", chIdDict);
+        return chIdDict;
+    } catch (error) {
+        console.error("请求失败: " + error.message);
+        return {};
     }
 }
 export async function open_channel_product_list(chIds) {
@@ -145,165 +211,69 @@ export async function open_channel_product_list(chIds) {
         console.log("URL 不匹配, 跳过处理");
     }
 }
-export function export_tsc() {
-    // 首页导出数据组件
-    let html = `
-        <img src="https://aimg8.dlssyht.cn/u/1533835/ueditor/image/767/1533835/1746954291684901.png" id="toggleImg" />
-        <input type="text" id="nameInput" placeholder="请输入姓名" />
-        <img src="https://aimg8.dlssyht.cn/u/1533835/ueditor/image/767/1533835/1747970937415077.png" id="clearLocalStorage" />
-        <input type="text" id="usernameInput" value="当前用户名: ">
-        <button id="exportx">导出数据为 xlsx</button>
-        `;
-    $("body").append(html);
-    if (url.includes("denglu.php")) { $("#toggleImg").css("display", "none"); }
-    let autorun = Number(localStorage.getItem("autorun"));
-    if (autorun) {
-        $("#toggleImg").attr("src", "https://aimg8.dlssyht.cn/u/1533835/ueditor/image/767/1533835/1746954291684901.png");
+export async function openProductsEdit() {
+    // ESC打开产品栏目管理列表页
+    let username = $(".welcome").text().split("欢迎您：")[1].trim();
+    await publics.appendToRecord("usernames", username);
+    let page = publics.getUrlParameter(url, "page");
+    $("a").filter(function () {
+        return $(this).text().trim() === "编辑";
+    }).each(function (index) {
+        window.open($(this).attr("href") + "#page=" + page + "&num=" + (index + 1), "_blank");
+    });
+    if ($(".page-next").length) {
+        location.href = $(".page-next").attr("href");
+    } else {
+        window.close();
     }
-    else {
-        $("#toggleImg").attr("src", "https://aimg8.dlssyht.cn/u/1533835/ueditor/image/767/1533835/1746954290554632.png");
-    }
-    $("#toggleImg").click(function () {
-        let autorun = Number(localStorage.getItem("autorun"));
-        if (autorun) {
-            $(this).attr("src", "https://aimg8.dlssyht.cn/u/1533835/ueditor/image/767/1533835/1746954290554632.png");
-            autorun = 0;
-        } else {
-            $(this).attr("src", "https://aimg8.dlssyht.cn/u/1533835/ueditor/image/767/1533835/1746954291684901.png");
-            autorun = 1;
+};
+export async function handleProductAction(checked_car, status = "") {
+    let today = publics.generateTimestamp(0);
+    let person = "xxpersonname";
+    let username = $(".welcome").text().trim().replace("欢迎您：", "");
+    let ch_id = publics.getUrlParameter(url, "ch_id");
+    let id = publics.getUrlParameter(url, "id");
+    let ch_name = $(".myColumnTit").text();
+    let product_link = `http://testpage.qipeiyigou.com/qipeiyigouwang/products/${id}.html${location.hash}`;
+    let page = publics.getUrlParameter(url, "page");
+    let num = publics.getUrlParameter(url, "num");
+    let record = `${today}\t${person}\t${username}\t${ch_id}\t${id}\t${ch_name}\t${product_link}\t${page}\t${num}\t`;
+    let labelsBefore = Array.from(document.querySelectorAll(`input[name="properties[]"]:checked`))
+        .map(checkbox => checkbox.closest("label").textContent.trim())
+        .join(", ");
+    if (status === "未处理") {
+        record += `${labelsBefore}\t${labelsBefore}\t${status}`;
+        await publics.appendToRecord("record", record);
+        window.close();
+    } else {
+        async function processing(elements) {
+            $("input[type=checkbox][value=4]").prop("checked", false);
+            if (checked_car) {
+                $("input[type=checkbox][value=2]").prop("checked", true);
+            }
+            let labelsAfter = Array.from(document.querySelectorAll(`input[name="properties[]"]:checked`))
+                .map(checkbox => checkbox.closest("label").textContent.trim())
+                .join(", ");
+            record += `${labelsBefore}\t${labelsAfter}\t${status}`;
+            await publics.appendToRecord("record", record);
+            $("title").text("完成");
+            $("#submit_msg a").click();
         }
-        localStorage.setItem("autorun", autorun);
-    });
-    let stored_personname = localStorage.getItem("name");
-    if (stored_personname) {
-        $("#nameInput").val(stored_personname);
+        let selectors = ["#sub_id option:selected", "#shop_pro_class_big_id option:selected"];
+        await publics.waitfor(selectors, 0, processing);
     }
-    let stored_usernames = localStorage.getItem("usernames");
-    if (stored_usernames) {
-        let first_stored_username = stored_usernames.split(" ")[0];
-        $("#commonName").val(first_stored_username);
-        $("#commonName").focus();
-        $("#commonName").prop("disabled", true);
-        $("#commonPassword").val("111111");
-        $("#commonPassword").focus();
-        $("#commonYzm").focus();
-        $("#usernameInput").val(`当前用户名: ${first_stored_username}`);
-    }
-    $("#exportx").click(function () {
-        let personName = $("#nameInput").val().trim();
-        localStorage.setItem("name", personName);
-        if (!personName) {
-            alert("姓名不能为空！");
-            return;
-        }
-        let time = publics.generateTimestamp(1);
-        let fileName = `${personName}-${time}`;
-        publics.downloadRecordAsXLSX(personName, fileName);
-    });
-    $("#clearLocalStorage").click(() => {
-        localStorage.clear();
-        $("#clearLocalStorage").attr("src", "https://aimg8.dlssyht.cn/u/1533835/ueditor/image/767/1533835/1747976911167008.png");
-        setTimeout(() => { location.reload(); }, 1000);
-    });
-    $("#usernameInput").click(() => { $("#usernameInput").val(""); });
-    $("#usernameInput").on("input", function () {
-        let usernames = $("#usernameInput").val().trim();
-        if (usernames.includes("当前用户名:")) {
-            usernames = usernames.split("当前用户名:")[1].trim();
-        }
-        let first_username = usernames.split(" ")[0];
-        localStorage.setItem("usernames", usernames);
-        $("#commonName").val(first_username);
-        $("#commonName").focus();
-        $("#commonName").prop("disabled", true);
-        $("#commonPassword").val("111111");
-        $("#commonPassword").focus();
-        $("#commonYzm").focus();
-        $("#usernameInput").val(`当前用户名: ${first_username}`);
-    });
 }
-export function showKeyword() {
-    // 显示关键词
-    let keyword = $("meta[name=keywords]").attr("content");
-    let author = $("meta[name=author]").attr("content");
-    if (keyword.includes(author)) {
-        $("#keywordx").text("关键词为空");
-    } else {
-        $("#keywordx").text(keyword);
-    }
-};
-export function checkProduct() {
-    // 检查产品详情
-    let id = 0;
-    let tip = "";
-    $("#tipx").text("正在检查中……");
-    if ($(".main a").length) {
-        id = 1;
-        tip += "存在超链接！";
-    }
-    if ($(".main *[style*=pointer]").length) {
-        id = 2;
-        tip += "存在非超链接小手！";
-    }
-    let images = $(".main img");
-    for (let i = 0; i < images.length; i++) {
-        let src = images.eq(i).attr("src");
-        if (!src.includes("aimg8.dlssyht.cn")) {
-            id = 3;
-            tip += "存在外链图片！";
-            break;
-        }
-    }
-    if (id === 0) {
-        tip = "正常！";
-    } else {
-        $("#tipx").css("background-color", "red");
-        alert(tip);
-    }
-    $("#tipx").text(`检查结果: ${tip}`);
-};
-function extractDataAsObject() {
+export function extractDataAsObject() {
     // 手动提取商家中心的栏目{id:名称}
     let items = document.querySelectorAll(".item-list li");
     let dataObj = {};
     items.forEach(item => {
         let link = item.querySelector("a");
-        let chId = new URLSearchParams(link.search).get("ch_id");
+        let chId = publics.getUrlParameter(link, "ch_id");
         let title = item.querySelector(".p-tit").textContent.trim();
         dataObj[chId] = title;
     });
     console.log(dataObj);
     return dataObj;
 }
-export async function fetchChIdsAndTitles(url) {
-    try {
-        const response = await fetch(url);
-        if (!response.ok) {
-            throw new Error(`请求失败, 状态码: ${response.status}`);
-        }
-        const arrayBuffer = await response.arrayBuffer();
-        const decoder = new TextDecoder("gbk");
-        const decodedText = decoder.decode(arrayBuffer);
-        const parser = new DOMParser();
-        const doc = parser.parseFromString(decodedText, "text/html");
-        const anchorElements = doc.querySelectorAll(`a[href*="ch_id="]`);
-        const chIdDict = {};
-        anchorElements.forEach(anchor => {
-            const chIdMatch = anchor.href.match(/ch_id=(\d+)/);
-            if (chIdMatch) {
-                const chId = chIdMatch[1];
-                const titleElement = anchor.querySelector(".p-tit");
-                const title = titleElement ? titleElement.textContent.trim() : "";
-                if (title) {
-                    chIdDict[chId] = title;
-                }
-            }
-        });
-        console.log("提取到的 ch_id 和标题字典: ", chIdDict);
-        return chIdDict;
-    } catch (error) {
-        console.error("请求失败: " + error.message);
-        return {};
-    }
-}
-// End-309-2025.05.23.175600
+// End-279-2025.05.25.132241
