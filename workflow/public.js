b30a2b7bb1b87e40039caf05fb7a8c11bedd8cc9
1024nettech <123078217+1024nettech@users.noreply.github.com>
2025-05-27 14:14:26 +0800

    Update public.js

--- workflow/public.js
+++ workflow/public.js
@@ -220,7 +220,7 @@ export async function waitfor(selectors, delayTime, doCallback) {
             }
         });
         let config = { childList: true, subtree: true };
-        observer.observe("#form1", config);
+        observer.observe(document.body, config);
     });
 }
 export function parseJson(jsonString) {
@@ -332,4 +332,4 @@ export async function downloadRecordAsFile(personName, fileName) {
     XLSX.writeFile(wb, `${fileName}.xlsx`);
     console.log("XLSX 文件已生成并开始下载");
 }
-// End-335-2025.05.27.025422
+// End-335-2025.05.27.141359
