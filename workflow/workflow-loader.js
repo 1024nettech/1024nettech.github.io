e10a22eb8d7aaa9a8795399373ed84a5cbf0a061
1024nettech <123078217+1024nettech@users.noreply.github.com>
2025-05-27 08:34:23 +0800

    Update workflow-loader.js

--- workflow/workflow-loader.js
+++ workflow/workflow-loader.js
@@ -1,4 +1,4 @@
-import * as publics from `./public.js`
+import * as publics from "./public.js"
 const url = location.href;
 const autorun = Number(localStorage.getItem("autorun"));
 if (autorun) {
@@ -47,4 +47,4 @@ function update() {
 }
 let version_url = `https://1024nettech.github.io/workflow/version.json?t=${Date.now()}`;
 update();
-// End-50-2025.05.27.083102
+// End-50-2025.05.27.083402
