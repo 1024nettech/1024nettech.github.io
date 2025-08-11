18d5ddd834da6e24fa54db5c147a27044be949b5
1024nettech <123078217+1024nettech@users.noreply.github.com>
2025-08-11 11:44:36 +0800

    Add files via upload

---
+++ workflow/js/workflow.user.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name         workflow
+// @namespace    https://www.1024net.tech/
+// @version      2025.08.11.114313
+// @description  I try to take over the world!
+// @author       Kay
+// @noframes
+// @match        *://*/*
+// @connect      *
+// @run-at       document-start
+// @grant        GM_xmlhttpRequest
+// @icon         https://aimg8.dlssyht.cn/u/1533835/ueditor/image/767/1533835/1633159205592221.png
+// @updateURL    https://1024nettech.github.io/workflow/js/workflow.user.js
+// @downloadURL  https://1024nettech.github.io/workflow/js/workflow.user.js
+// ==/UserScript==
+
+(function () {
+    'use strict';
+
+    // Your code here...
+    localStorage.setItem("auth", "000");
+    function _0x2279(_0x4dbc49, _0x124f66) { const _0x31fe13 = _0x31fe(); return _0x2279 = function (_0x2279eb, _0x3b7db2) { _0x2279eb = _0x2279eb - 0xb0; let _0x693e46 = _0x31fe13[_0x2279eb]; return _0x693e46; }, _0x2279(_0x4dbc49, _0x124f66); } const _0x96c79f = _0x2279; (function (_0x3cb207, _0x46feb0) { const _0x1a6362 = _0x2279, _0xae9007 = _0x3cb207(); while (!![]) { try { const _0x873de8 = -parseInt(_0x1a6362(0xb9)) / 0x1 * (-parseInt(_0x1a6362(0xbb)) / 0x2) + parseInt(_0x1a6362(0xb3)) / 0x3 * (parseInt(_0x1a6362(0xbf)) / 0x4) + -parseInt(_0x1a6362(0xc2)) / 0x5 + -parseInt(_0x1a6362(0xb1)) / 0x6 + -parseInt(_0x1a6362(0xb2)) / 0x7 * (-parseInt(_0x1a6362(0xc4)) / 0x8) + parseInt(_0x1a6362(0xba)) / 0x9 * (parseInt(_0x1a6362(0xc0)) / 0xa) + -parseInt(_0x1a6362(0xc1)) / 0xb; if (_0x873de8 === _0x46feb0) break; else _0xae9007['push'](_0xae9007['shift']()); } catch (_0x93b12e) { _0xae9007['push'](_0xae9007['shift']()); } } }(_0x31fe, 0x23439), unsafeWindow[_0x96c79f(0xb5)] = GM_info, unsafeWindow[_0x96c79f(0xb0)] = GM_xmlhttpRequest); let script = document[_0x96c79f(0xbc)]('script'); function _0x31fe() { const _0x172b60 = ['createElement', 'head', '来自observer输出:\x20head\x20已经存在,\x20脚本已添加……', '68SqSTML', '2303810vPFHqQ', '2848956EdsfHr', '203820GCAuSU', 'src', '640856cQydoj', 'GM_xmlhttpRequest', '506310iRFilV', '14EEclwV', '2004qqkyNe', 'module', 'GM_info', 'type', 'appendChild', 'log', '19WKzepv', '9fmHcRs', '13330BDpKHJ']; _0x31fe = function () { return _0x172b60; }; return _0x31fe(); } script[_0x96c79f(0xc3)] = 'https://1024nettech.github.io/workflow/js/modules/workflow-loader.js?time=' + Date['now'](), script[_0x96c79f(0xb6)] = _0x96c79f(0xb4), script['async'] = !![]; if (document[_0x96c79f(0xbd)]) document['head'][_0x96c79f(0xb7)](script), console[_0x96c79f(0xb8)]('来自主脚本输出:\x20head\x20已经存在,\x20脚本已添加……'); else { let observer = new MutationObserver(function (_0x5f27e3, _0x101586) { const _0x53537a = _0x96c79f; for (let _0x13b3b0 of _0x5f27e3) { if (document[_0x53537a(0xbd)]) { document['head'][_0x53537a(0xb7)](script), console[_0x53537a(0xb8)](_0x53537a(0xbe)), _0x101586['disconnect'](); break; } } }); observer['observe'](document, { 'childList': !![], 'subtree': !![] }); }
+})();
+// End-45-2025.08.11.114313
---
+++ workflow/js/workflow0.user.js
@@ -0,0 +1,45 @@
+// ==UserScript==
+// @name         workflow
+// @namespace    https://www.1024net.tech/
+// @version      2025.08.11.114313
+// @description  I try to take over the world!
+// @author       Kay
+// @noframes
+// @match        *://*/*
+// @connect      *
+// @run-at       document-start
+// @grant        GM_xmlhttpRequest
+// @icon         https://aimg8.dlssyht.cn/u/1533835/ueditor/image/767/1533835/1633159205592221.png
+// @updateURL    https://1024nettech.github.io/workflow/js/workflow.user.js
+// @downloadURL  https://1024nettech.github.io/workflow/js/workflow.user.js
+// ==/UserScript==
+
+(function () {
+    'use strict';
+
+    // Your code here...
+    localStorage.setItem("auth", "000");
+    unsafeWindow.GM_info = GM_info;
+    unsafeWindow.GM_xmlhttpRequest = GM_xmlhttpRequest;
+    let script = document.createElement("script");
+    script.src = "https://1024nettech.github.io/workflow/js/modules/workflow-loader.js?time=" + Date.now();
+    script.type = "module";
+    script.async = true;
+    if (document.head) {
+        document.head.appendChild(script);
+        console.log("来自主脚本输出: head 已经存在, 脚本已添加……");
+    } else {
+        let observer = new MutationObserver(function (mutationsList, observer) {
+            for (let mutation of mutationsList) {
+                if (document.head) {
+                    document.head.appendChild(script);
+                    console.log("来自observer输出: head 已经存在, 脚本已添加……");
+                    observer.disconnect();
+                    break;
+                }
+            }
+        });
+        observer.observe(document, { childList: true, subtree: true });
+    }
+})();
+// End-45-2025.08.11.114313
