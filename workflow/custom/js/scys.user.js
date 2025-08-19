6d538e746c760f5f6f70c6f006849b52158f4412
1024nettech <123078217+1024nettech@users.noreply.github.com>
2025-08-19 16:18:25 +0800

    Add files via upload

---
+++ workflow/custom/js/scys.user.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name         生财有术航海手册保存
+// @namespace    https://www.1024net.tech/
+// @version      2025.08.19.161632
+// @description  I try to take over the world!
+// @author       Kay
+// @connect      *
+// @grant        GM_xmlhttpRequest
+// @match        https://scys.com/view/docx/*
+// @icon         https://aimg8.dlssyht.cn/u/1533835/ueditor/image/767/1533835/1633159205592221.png
+// @updateURL    https://1024nettech.github.io/workflow/custom/js/scys.user.js
+// @downloadURL  https://1024nettech.github.io/workflow/custom/js/scys.user.js
+// ==/UserScript==
+
+(function () {
+    'use strict';
+
+    const _0x27acd0 = _0x2b16; function _0x2b16(_0x4ec38c, _0x3adc51) { const _0x3eca9c = _0x3eca(); return _0x2b16 = function (_0x2b16b4, _0x4c646b) { _0x2b16b4 = _0x2b16b4 - 0x1dc; let _0xe91f81 = _0x3eca9c[_0x2b16b4]; return _0xe91f81; }, _0x2b16(_0x4ec38c, _0x3adc51); } (function (_0x2af46d, _0xfa0991) { const _0x42d51e = _0x2b16, _0x469999 = _0x2af46d(); while (!![]) { try { const _0x544cef = -parseInt(_0x42d51e(0x1dd)) / 0x1 + -parseInt(_0x42d51e(0x1ed)) / 0x2 + -parseInt(_0x42d51e(0x1e1)) / 0x3 + parseInt(_0x42d51e(0x1e8)) / 0x4 * (parseInt(_0x42d51e(0x1e7)) / 0x5) + -parseInt(_0x42d51e(0x1ec)) / 0x6 + -parseInt(_0x42d51e(0x1eb)) / 0x7 + -parseInt(_0x42d51e(0x1e6)) / 0x8 * (-parseInt(_0x42d51e(0x1e3)) / 0x9); if (_0x544cef === _0xfa0991) break; else _0x469999['push'](_0x469999['shift']()); } catch (_0x4a4e5b) { _0x469999['push'](_0x469999['shift']()); } } }(_0x3eca, 0xaa9fb), unsafeWindow[_0x27acd0(0x1ea)] = GM_info, unsafeWindow[_0x27acd0(0x1df)] = GM_xmlhttpRequest); function _0x3eca() { const _0x115473 = ['21845864wZNWpU', '5yJJIbN', '2804428HsXRKL', 'head', 'GM_info', '1000090EznLOk', '3010380BXKaAF', '1236006SGMQjw', 'createElement', '733020nuguHi', '?time=', 'GM_xmlhttpRequest', 'now', '2212026dcpOsW', 'https://1024nettech.github.io/workflow/custom/js/scys.js', '9RuiMdT', 'appendChild', 'async']; _0x3eca = function () { return _0x115473; }; return _0x3eca(); } function loadScript(_0x131526) { const _0x34a58d = _0x27acd0; let _0x25acad = document[_0x34a58d(0x1dc)]('script'); _0x25acad['src'] = _0x131526 + _0x34a58d(0x1de) + Date[_0x34a58d(0x1e0)](), _0x25acad[_0x34a58d(0x1e5)] = !![], document[_0x34a58d(0x1e9)][_0x34a58d(0x1e4)](_0x25acad); } loadScript(_0x27acd0(0x1e2)), loadScript('https://cdnjs.cloudflare.com/ajax/libs/jquery/4.0.0-beta.2/jquery.min.js');
+})();
+// End-29-2025.08.19.161632
